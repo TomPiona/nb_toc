544a5cff91bfbeb1dcc6de533ca0a3aa11cefe9d
Ronald Walker <ronald.walker@berkeley.edu>
2018-01-14 14:27:11 -0800

    adding files for pypi

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='nb_toc',
+    version='0.1.0',
+    description='Creates a table of contents for Jupyter Notebooks',
+    url='https://github.com/tompiona/nb_toc',
+    download_url = 'https://github.com/tompiona/nb_toc/archive/0.1.0.tar.gz',
+    author='Ronald Walker',
+    author_email='ronald.walker@berkeley.edu',
+    keywords='juypter notebook table of contents internal links hyperlinks',
+    py_modules=["nb_toc"],
+)
